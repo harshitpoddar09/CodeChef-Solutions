e378f1c2b800f1ce7529401e6f74047914504e3a
Harshit Poddar <53508456+harshitpoddar09@users.noreply.github.com>
2021-04-15 10:22:43 +0530

    Add files via upload
    SNCKYEAR

---
+++ Beginner/Chef and SnackDown.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t=int(input())
+for i in range(t):
+    n=int(input())
+    hosted=[2010,2015,2016,2017,2019]
+    if n in hosted:
+        print('HOSTED')
+    else:
+        print('NOT HOSTED')
